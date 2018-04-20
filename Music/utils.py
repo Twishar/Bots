90640ff92043435b67b67bc089df51ba1c2b8c7e
Vladislav Stognienko <stognienkovv@gmail.com>
2018-04-20 17:34:57 +0300

    Fix

--- Music/utils.py
+++ Music/utils.py
@@ -1,5 +1,9 @@
 
 import shelve
+from random import shuffle
+
+from telebot.types import ReplyKeyboardMarkup
+
 from SQLighter import SQLighter
 from config import shelve_name, database_name
 
@@ -57,3 +61,30 @@ def get_answer_for_user(chat_id):
             return answer
         except KeyError:
             return None
+
+
+def generate_markup(right_answer, wrong_answers):
+    """
+    Создаем кастомную клавиатуру для выбора ответа
+    :param right_answer: Правильный ответ
+    :param wrong_answers: Набор неправильных ответов
+    :return: Обьект кастомной клавиатуры
+    """
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+
+    # Склеиваем правильный ответ с неправильными
+    all_answers = '{}, {}'.format(right_answer, wrong_answers)
+
+    # Создаем лист (массив) и записываем в него все элементы
+    list_items = []
+    for item in all_answers.split(','):
+        list_items.append(item)
+
+    # Хорошенько перемешаем все элементы
+    shuffle(list_items)
+
+    # Заполняем разметку перемешанными элементами
+    for item in list_items:
+        markup.add(item)
+
+    return markup
