1bdec4823b23ec847cf3da02f69cd468f3651e8e
Vladislav Stognienko <stognienkovv@gmail.com>
2018-04-26 14:48:28 +0300

    test bot

---
+++ python_vs_js_bot/bot.py
@@ -0,0 +1,41 @@
+import config
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
+import apiai
+import json
+
+
+updater = Updater(token=config.token)
+dispatcher = updater.dispatcher
+
+
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Дарова, щеглы')
+
+
+def textMessage(bot, update):
+    request = apiai.ApiAI(config.flow_token).text_request()  # Токен API к DialogFlow
+    request.lang = 'ru'  # На каком языке будет послан запрос
+    request.session_id = '@twishar_test_api_bot'
+    request.query = update.message.text   # Посылаем запрос к ИИ с сообщением от юзера
+    responseJson = json.loads(request.getresponse().read().decode('utf-8'))
+    response = responseJson['result']['fulfillment']['speech']      # Разбираем JSON и вытаскиваем ответ
+
+    # Если есть ответ от бота - присылаем юзеру, если нет - бот его не понял
+    if response:
+        bot.send_message(chat_id=update.message.chat_id, text=response)
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text='Чет какую-то дичь вы несете')
+
+
+# Хендлеры
+start_command_handler = CommandHandler('start', startCommand)
+text_message_handler = MessageHandler(Filters.text, textMessage)
+
+# Добавляем хендлеры в диспетчер
+dispatcher.add_handler(start_command_handler)
+dispatcher.add_handler(text_message_handler)
+
+# Начинаем поиск обновлений
+updater.start_polling(clean=True)
+
+updater.idle()
