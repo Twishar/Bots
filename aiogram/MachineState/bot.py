a6db378cbb0ecc4a1ccecf19541bf2180004404a
Vladislav Stognienko <stognienkovv@gmail.com>
2018-05-02 14:01:57 +0300

    shutdown dispatcher

--- aiogram/MachineState/bot.py
+++ aiogram/MachineState/bot.py
@@ -68,8 +68,9 @@ async def echo_message(message: types.Message):
     await bot.send_message(message.from_user.id, message.text)
 
 
-
-
+async def shutdown(dispatcher: Dispatcher):
+    await dispatcher.storage.close()
+    await dispatcher.storage.wait_closed()
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, on_shutdown=shutdown)
